f32e6b9abcf6252b3906430f706086c98d177705
angyyang <angy_yang@hotmail.com>
2024-06-28 13:46:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import signIn from './sign_in.js'
 var now = new Date();
 signIn()
   .then(() => {
-    console.log('执行签到成功！ ' , now ) 
+    console.log('执行签到成功！ ' , now  ) 
   })
   .catch(error => {
     console.error('执行签到失败：', error.message, now )
